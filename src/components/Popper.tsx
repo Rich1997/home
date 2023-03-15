1fd47a3479b5168068c15b6042411cdc32cb563c
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-03-15 23:37:44 +0530

    2 files modified

--- src/components/Popper.tsx
+++ src/components/Popper.tsx
@@ -8,10 +8,10 @@ const Popper = () => {
         <>
             <button
                 title="Settings"
-                className={`z-50 fixed laptop:top-8 top-4 laptop:right-8 right-4 rounded-full font-medium hover:text-opacity-100 focus:outline-0`}
+                className={`z-50 fixed top-8 right-6 rounded-full font-medium hover:text-opacity-100 focus:outline-0`}
                 onClick={() => setIsOpen(!isOpen)}
             >
-                <div className="flex items-center justify-center h-8 w-8 rounded-full default-bg b dark:text-tertiary-dark text-tertiary-light d-h hover:animate-spin-soft">
+                <div className="flex items-center justify-center h-7 w-7 rounded-full default-bg b dark:text-tertiary-dark text-tertiary-light d-h hover:animate-spin-soft">
                     <SettingsIcon size={24} />
                 </div>
             </button>
@@ -20,36 +20,38 @@ const Popper = () => {
                     isOpen ? 'fixed' : 'hidden'
                 }`}
             >
-                <div className="p-4 pt-8 flex flex-col gap-4 ">
-                    <div className="font-bold text-lg default-text">
-                        Settings
-                    </div>
-                    <div className="flex flex-col gap-2 items-start cursor-default">
-                        <div>Theme</div>
-                        <List />
-                    </div>
-                    <div className="flex flex-col gap-2 items-start cursor-default">
-                        <div>Font</div>
-                    </div>
-                </div>
-                <div className="p-6 default-text dark:bg-surface-dark bg-surface-light">
-                    <span className="flex justify-between items-center gap-4">
-                        <span>home version 2.0</span>
-                        <div className="flex gap-4">
-                            <a
-                                href="##"
-                                className="transition duration-150 ease-in-out"
-                            >
-                                {/* <GitHubIcon size={16} /> */}
-                            </a>
-                            <a
-                                href="##"
-                                className="transition duration-150 ease-in-out"
-                            >
-                                {/* <LinkedinIcon size={16} /> */}
-                            </a>
+                <div className="flex flex-col justify-between h-full">
+                    <div className="pt-8 flex flex-col gap-4 ">
+                        <div className="px-6 pb-6 font-bold text-lg default-text b-b">
+                            Settings
                         </div>
-                    </span>
+                        <div className="px-6 flex flex-col gap-2 items-start cursor-default">
+                            <div>Theme</div>
+                            <List />
+                        </div>
+                        <div className="px-6 flex flex-col gap-2 items-start cursor-default">
+                            <div>Font</div>
+                        </div>
+                    </div>
+                    <div className="p-6 default-text dark:bg-surface-dark bg-surface-light">
+                        <span className="flex justify-between items-center gap-4">
+                            <span>home version 2.0</span>
+                            <div className="flex gap-4">
+                                <a
+                                    href="##"
+                                    className="transition duration-150 ease-in-out"
+                                >
+                                    {/* <GitHubIcon size={16} /> */}
+                                </a>
+                                <a
+                                    href="##"
+                                    className="transition duration-150 ease-in-out"
+                                >
+                                    {/* <LinkedinIcon size={16} /> */}
+                                </a>
+                            </div>
+                        </span>
+                    </div>
                 </div>
             </div>
         </>
