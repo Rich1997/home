5eec2f44cf8585652761e3bd63315f27b3988ebc
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-03-25 13:00:31 +0530

    2 files modified, 1 added

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
+import './fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
